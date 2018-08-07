32ccd1f37f3f45ca27e17fbe28dc158656200057
Javier <javier.jara@logn.co>
2018-08-07 17:12:31 -0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new', as: 'signup'
-  # same result as 
-  # get 'signup', to: 'users#new'
-
+  # get 'users/new', as: 'signup'
+  # same result as -- Not working when exists resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to:'users#create'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do 
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do 
+      post users_path, params: {user: { name: "javi", email: "user@invalid", password: "foo", password_confirmation: "foo"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path 
+    assert_select 'form[action="/signup"]'
+
+    assert_difference "User.count", 1 do 
+      post users_path, params: {user: { name: "javillo", email: "javierjaram@gmail.com", password: "m0bile_qa", password_confirmation: "m0bile_qa"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div#alert alert-success'
+  end
+end
